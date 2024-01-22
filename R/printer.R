d395b21ec1a6a0fce26c26079214758ed77d3786
Sanne Govaert <sangovae@Boswilg.UGent.be>
2024-01-22 19:04:25 +0100

    commit

--- R/printer.R
+++ R/printer.R
@@ -1,14 +1,20 @@
-#' Title
+#' Silly printer function
 #'
-#' @param r
-#' @param x
+#' @param r what you want in the second column
+#' @param x what you want in the first column
 #' @param y
 #'
-#' @return
+#' @return A tibble
 #' @export
 #'
+#' @importFrom tibble data_frame
+#' @importFrom utils head adist
+#' @importFrom dplyr select
 #' @examples
-printer = function(r, x, y){
-  print(paste0("x = ", x))
+#' printer(x = rnorm(5), r = rnorm(5))
+printer = function(r, x){
+  x = data_frame(x =x, r= r)
+  print(head(x))
+  return(x)
 }
-use_readme_md()
+
