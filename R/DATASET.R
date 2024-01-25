35edecf96b1371a38e8d514be52d91113e23a7fd
Sanne Govaert <sangovae@Boswilg.UGent.be>
2024-01-25 21:36:48 +0100

    - unit test - replace data.frame by tibble - adding data to package

---
+++ R/DATASET.R
@@ -0,0 +1,10 @@
+#' @title Sample Data for Analysis
+#'
+#' @description A simple \code{tibble} that is for simple testing and running of examples
+#'
+#' @format A \code{tibble} with 2 columns, which are:
+#' \describe{
+#'  \item{x}{random normal data}
+#'  \item{y}{outcome that is normal based on \code{x}}
+#'  }
+"DATASET"
---
+++ R/sample_read.R
@@ -0,0 +1,15 @@
+#' Read CSV duplicate
+#'
+#' @param path path to filename
+#'
+#' @return a \code{tibble}
+#' @export
+#' @importFrom readr read_csv
+#'
+#' @examples
+#' csv = system.file("extdata", "sample_data.csv", package = "tester")
+#' sample_read(csv)
+sample_read = function(path){
+  readr::read_csv(path)
+
+}
